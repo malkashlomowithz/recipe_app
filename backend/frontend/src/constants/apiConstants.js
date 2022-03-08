7ad289d447aa3f8c344ab2e56abaa727fe367b44
malkashlomowithz <61754416+malkashlomowithz@users.noreply.github.com>
2022-03-08 20:42:45 +0200

    Update apiConstants.js

--- backend/frontend/src/constants/apiConstants.js
+++ backend/frontend/src/constants/apiConstants.js
@@ -1,6 +1,6 @@
-export const API_BASE_URL_AUTH =`https://my-recipe-app-1fe491.appdrag.site/api/auth`;
-export const API_BASE_URL_ADMIN =`https://my-recipe-app-1fe491.appdrag.site/api/admin`;
-export const API_BASE_URL_PREMIUM =`https://my-recipe-app-1fe491.appdrag.site/api/premium`;
-export const API_BASE_URL_RECIPES = `http://localhost:8080/api/recipe`;
-export const API_BASE_URL_PUBLISH = `http://localhost:8080/api/publish`;
-export const API_BASE_URL = `http://localhost:8080/api`
+export const API_BASE_URL_AUTH =``;
+export const API_BASE_URL_ADMIN =``;
+export const API_BASE_URL_PREMIUM =``;
+export const API_BASE_URL_RECIPES = ``;
+export const API_BASE_URL_PUBLISH = ``;
+export const API_BASE_URL = ``;
